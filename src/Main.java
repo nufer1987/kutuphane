57d8ca277cbedc7a97cebd312eb952001503a0e5
nufer <simseknufer1987@gmail.com>
2021-04-08 17:19:05 +0300

    code OK

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Runner runner=new Runner();
 
-        Kitap ktp=new Kitap("nazım","haluk","1999",1002);
+        Kitap ktp=new Kitap("nazım","haluk","1999",1001);
         Kitap ktp2=new Kitap("nazım","haluk","1999",1002);
 
-        ktpLst.add(0,ktp2);
-        ktpLst.add(1,ktp);
+        ktpLst.add(0,ktp);
+        ktpLst.add(1,ktp2);
 
-        System.out.println(ktpLst.get(1).fiyat);
+//        System.out.println(ktpLst.get(1).fiyat);
 
         runner.uygulama();
     }
